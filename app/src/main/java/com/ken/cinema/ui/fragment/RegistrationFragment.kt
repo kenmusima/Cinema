7baba1520048983c26784cf3a1f859fb0998b02a
kenmusima <ksmusima@gmail.com>
2022-01-24 14:29:17 +0300

    Registration and Login touches with firebase.

--- app/src/main/java/com/ken/cinema/ui/fragment/RegistrationFragment.kt
+++ app/src/main/java/com/ken/cinema/ui/fragment/RegistrationFragment.kt
@@ -1,16 +1,24 @@
 package com.ken.cinema.ui.fragment
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ken.cinema.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
 
     private lateinit var binding: FragmentRegistrationBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +26,77 @@ class RegistrationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
+        binding.register.setOnClickListener {
+            var (emailValid, passwordValid) = validateFields()
+            createUser(emailValid, passwordValid)
+        }
         return binding.root
+    }
+
+
+    private fun validateFields(): Pair<Boolean, Boolean> {
+        var email = binding.emailRegistration.text.toString().trim()
+        var password = binding.passwordRegistration.text.toString().trim()
+        var confirmPassword = binding.confirmPasswordRegistration.text.toString().trim()
+        var isEmailValid: Boolean = false
+        var isPasswordValid: Boolean = false
+
+        isEmailValid = if (TextUtils.isEmpty(email)) {
+            Toast.makeText(activity, "Please enter email", Toast.LENGTH_SHORT).show()
+            false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(activity, "Invalid Email Address", Toast.LENGTH_SHORT).show()
+            false
+        } else {
+            true
+        }
+
+        isPasswordValid = when {
+            TextUtils.isEmpty(password) -> {
+                Toast.makeText(activity, "Please enter a password", Toast.LENGTH_SHORT).show()
+                false
+            }
+            password.length < 6 -> {
+                Toast.makeText(activity, "Password length should be greater than 6", Toast.LENGTH_SHORT)
+                    .show()
+                false
+            }
+            password != confirmPassword -> {
+                Toast.makeText(activity, "Password don't match", Toast.LENGTH_SHORT).show()
+                false
+            }
+            else -> {
+                true
+            }
+        }
+
+        return Pair(isEmailValid, isPasswordValid)
+
+    }
+
+    private fun createUser(emailValid: Boolean, passwordValid: Boolean) {
+
+        if (emailValid && passwordValid) {
+            var email = binding.emailRegistration.text.toString().trim()
+            var password = binding.passwordRegistration.text.toString().trim()
+
+            binding.progressCircular.visibility = View.VISIBLE
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success
+                        val action = RegistrationFragmentDirections.actionRegistrationFragmentToSignInFragment()
+                        findNavController().navigate(action)
+
+                    } else {
+                        // Sign in fails
+                        binding.progressCircular.visibility = View.GONE
+                        Toast.makeText(activity, "User Registration Failed.", Toast.LENGTH_SHORT)
+                            .show()
+
+                    }
+                }
+        }
     }
 }
--- app/src/main/java/com/ken/cinema/ui/fragment/SignInFragment.kt
+++ app/src/main/java/com/ken/cinema/ui/fragment/SignInFragment.kt
@@ -5,19 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ken.cinema.R
 import com.ken.cinema.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment(R.layout.fragment_sign_in) {
 
     private lateinit var binding: FragmentSignInBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSignInBinding.inflate(inflater,container,false)
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
+        binding.signInButton.setOnClickListener {
+            signIn()
+        }
+
         return binding.root
+    }
+
+
+    private fun signIn() {
+
+        var email = binding.signInEmail.text.toString().trim()
+        var password = binding.signInPassword.text.toString().trim()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+
+                } else {
+                    // Sign in fails
+                    Toast.makeText(activity, "Incorrect password or email.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
     }
 }
